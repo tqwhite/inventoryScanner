8dff5bae0dd1db6fbc79292c804e012e4508c604
TQ White II <tq@justkidding.com>
2019-10-13 18:22:20 -0500

    added intventory schema stuff

--- scanServer/scanServer.js
+++ scanServer/scanServer.js
@@ -175,12 +175,10 @@ console.dir({"saveDataResult [scanServer.js.constructSubStatus]":saveDataResult}
 	showCode='...'+dataModel.scanCode.substring(len-4, len);
 	
 	if (saveDataResult && saveDataResult.inventoryQtyOut && saveDataResult.inventoryQtyOut.length){
-console.log("saveDataResult.inventoryQtyOut[0].helixId="+saveDataResult.inventoryQtyOut[0].inStockAmount+" [scanServer.js.constructSubStatus]");
+console.log("saveDataResult.inventoryQtyOut[0].helixId="+saveDataResult.inventoryQtyOut[0].helixId+" [scanServer.js.constructSubStatus]");
 
-		var inventory="<!newLine!>In Stock: "+saveDataResult.inventoryQtyOut[0].inStockAmount;
+		var inventory="<!newLine!>In Stock: "+saveDataResult.inventoryQtyOut[0].helixId;
 	}
-console.log("inventory="+inventory+" [scanServer.js.constructSubStatus]");
-
 	else {
 		var inventory='';
 	}
